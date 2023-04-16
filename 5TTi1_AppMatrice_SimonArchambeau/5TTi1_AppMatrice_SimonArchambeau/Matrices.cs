d318ffa8df5957a36fd0af640164170de2bab2e8
Simon Archambeau <210303@site.asty.moulin.be>
2023-04-16 23:42:20 +0200

    programme fonctionnel a finir

--- 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Matrices.cs
+++ 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Matrices.cs
@@ -6,76 +6,75 @@ using System.Threading.Tasks;
 
 namespace _5TTi1_AppMatrice_SimonArchambeau
 {
-    class Matrices
+    public struct Matrices
     {
-		static void Matrice_tab_alea (string args)
+		public void Matrice_tab_alea (int nbLigne, int nbCol, out int[,] tabAlea)
         {
-			for (int ligne = 0; ligne <= nbLigne - 1; ligne += (1))
+			tabAlea = new int[nbLigne,nbCol];
+            Random alea = new Random();
+            for (int ligne = 0; ligne <= nbLigne - 1; ligne += (1))
 			{
 				for (int colonne = 0; colonne <= nbCol - 1; colonne += (1))
 				{
-					tabAlea[ligne, colonne] == alea.next(0, 20);
+					tabAlea[ligne, colonne] = alea.Next(0,20);
 				}
 			}
 		}
-
-
-        static void Matrice_addition (string args)
+        public void  Matrice_addition(int[,] Tab1, int[,] Tab2, out int[,] Tab3)
         {
-			bool addition == true;
+			bool addition = true;
+			Tab3 = new int[Tab1.GetLength(0), Tab1.GetLength(1)];
 			if (Tab1.GetLength(0) == Tab2.GetLength(0) && Tab1.GetLength(1) == Tab2.GetLength(1))
 			{
 				for (int ligne = 0; ligne <= Tab1.GetLength(0) - 1; ligne += (1))
 				{
 					for (int colonne = 0; colonne <= Tab1.GetLength(1) - 1; colonne += (1))
 					{
-						Tab3[ligne, colonne] == Tab1[ligne, colonne] + Tab2[ligne, colonne];
+						Tab3[ligne, colonne] = Tab1[ligne, colonne] + Tab2[ligne, colonne];
 					}
 				}
 			}
 			else
 			{
-				bool addition == false;
+				addition = false;
 			}
-		}
-
-		static void Matrice_concatenation (string args)
+        }
+		public void Matrice_concatenation (int[,] Tab, out String StTab)
         {
-			for (int ligne = 0; ligne <= Tab.Getlength(0) - 1; ligne += (1))
+			StTab= null;
+			for (int ligne = 0; ligne <= Tab.GetLength(0) - 1; ligne += (1))
 			{
 				for (int colonne = 0; colonne <= Tab.GetLength(1) - 1; colonne += (1))
 				{
-					stringTab +== Tab[ligne, colonne] + "|";
+					StTab += Tab[ligne, colonne] + "|";
 				}
-				stringTab[ligne, colonne] +== "\n";
+				StTab += "\n";
 			}
 		}
 
-		static void Matrice_multiplication (string args)
+		 public void Matrice_multiplication (int[,] matrice1, int[,] matrice2, out int[,] matriceMulti)
         {
-			int[,] matrice1;
-			int[,] matrice2;
-			int[,] matriceMulti;
-			bool test;
+			matriceMulti = new int[matrice1.GetLength(1), matrice2.GetLength(0)];
+			bool test = true;
 			if (matrice1.GetLength(1) == matrice2.GetLength(0))
 			{
-				test == true;
+				test = true;
 				for (int ligne = 0; ligne <= matrice1.GetLength(0) - 1; ligne += (1))
 				{
 					for (int colonne = 0; colonne <= matrice2.GetLength(1) - 1; colonne += (1))
 					{
-						matriceMulti[ligne, colonne] == 0;
+						matriceMulti[ligne, colonne] = 0;
 						for (int i = 0; i <= matrice1.GetLength(1) - 1; i += (1))
 						{
-							matriceMulti[ligne, colonne] +== matrice1[ligne, i] * matrice2[i, colonne];
+							matriceMulti[ligne, colonne] += matrice1[ligne, i] * matrice2[i, colonne];
 						}
 					}
 				}
 			}
 			else
 			{
-				test == false;
+				test = false;
 			}
-		}
+        }
     }
 }
--- 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Program.cs
+++ 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Program.cs
@@ -10,6 +10,20 @@ namespace _5TTi1_AppMatrice_SimonArchambeau
     {
         static void Main(string[] args)
         {
+            Matrices MesOutils = new Matrices();
+            int[,] t;
+            String StTab;
+            Console.WriteLine("Bienvenue dans mon programme Matrice !");
+            Console.Write("Veuillez saisir le nombre de lignes de la matrice : ");
+            int lignes = int.Parse(Console.ReadLine());
+            Console.Write("Veuillez saisir le nombre de colonnes de la matrice : ");
+            int colonnes = int.Parse(Console.ReadLine());
+            MesOutils.Matrice_tab_alea(lignes, colonnes,out t);
+            MesOutils.Matrice_concatenation(t, out StTab);
+            Console.WriteLine(StTab);
+            Console.WriteLine("Appuyez sur une touche pour quitter...");
+            Console.ReadKey();
+
         }
     }
 }
