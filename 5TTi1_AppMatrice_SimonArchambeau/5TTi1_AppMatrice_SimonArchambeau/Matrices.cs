2e34d37c2cdd0dc7e00928bdc470469356ca7cd9
Simon Archambeau <210303@site.asty.moulin.be>
2023-04-17 22:45:56 +0200

    version finale du programme

--- 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Matrices.cs
+++ 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Matrices.cs
@@ -6,76 +6,107 @@ using System.Threading.Tasks;
 
 namespace _5TTi1_AppMatrice_SimonArchambeau
 {
-    class Matrices
+    public struct Matrices
     {
-		static void Matrice_tab_alea (string args)
+		public void Matrice_tab_alea (int nbLigne, int nbCol, out int[,] tabAlea)
         {
-			for (int ligne = 0; ligne <= nbLigne - 1; ligne += (1))
+			tabAlea = new int[nbLigne,nbCol];
+            Random alea = new Random();
+            for (int ligne = 0; ligne <= nbLigne - 1; ligne += (1))
 			{
 				for (int colonne = 0; colonne <= nbCol - 1; colonne += (1))
 				{
-					tabAlea[ligne, colonne] == alea.next(0, 20);
+					tabAlea[ligne, colonne] = alea.Next(0,20);
 				}
 			}
 		}
-
-
-        static void Matrice_addition (string args)
+        public void  Matrice_addition(int[,] Tab1, int[,] Tab2, out int[,] Tab3, out Boolean addition)
         {
-			bool addition == true;
+			addition= new Boolean();
+			Tab3 = new int[Tab1.GetLength(0), Tab1.GetLength(1)];
 			if (Tab1.GetLength(0) == Tab2.GetLength(0) && Tab1.GetLength(1) == Tab2.GetLength(1))
 			{
 				for (int ligne = 0; ligne <= Tab1.GetLength(0) - 1; ligne += (1))
 				{
 					for (int colonne = 0; colonne <= Tab1.GetLength(1) - 1; colonne += (1))
 					{
-						Tab3[ligne, colonne] == Tab1[ligne, colonne] + Tab2[ligne, colonne];
+						Tab3[ligne, colonne] = Tab1[ligne, colonne] + Tab2[ligne, colonne];
 					}
 				}
 			}
 			else
 			{
-				bool addition == false;
+				addition = false;
 			}
-		}
-
-		static void Matrice_concatenation (string args)
+        }
+		public void Matrice_concatenation (int[,] Tab, out String StTab)
         {
-			for (int ligne = 0; ligne <= Tab.Getlength(0) - 1; ligne += (1))
+			StTab= null;
+			for (int ligne = 0; ligne <= Tab.GetLength(0) - 1; ligne += (1))
 			{
 				for (int colonne = 0; colonne <= Tab.GetLength(1) - 1; colonne += (1))
 				{
-					stringTab +== Tab[ligne, colonne] + "|";
+					StTab += Tab[ligne, colonne] + "|";
 				}
-				stringTab[ligne, colonne] +== "\n";
+				StTab += "\n";
 			}
 		}
 
-		static void Matrice_multiplication (string args)
+		public void Matrice_multiplication (int[,] matrice1, int[,] matrice2, out int[,] matriceMulti, out Boolean test)
         {
-			int[,] matrice1;
-			int[,] matrice2;
-			int[,] matriceMulti;
-			bool test;
+			matriceMulti = new int[matrice1.GetLength(1), matrice2.GetLength(0)];
+			test = new Boolean();
 			if (matrice1.GetLength(1) == matrice2.GetLength(0))
 			{
-				test == true;
+				test = true;
 				for (int ligne = 0; ligne <= matrice1.GetLength(0) - 1; ligne += (1))
 				{
 					for (int colonne = 0; colonne <= matrice2.GetLength(1) - 1; colonne += (1))
 					{
-						matriceMulti[ligne, colonne] == 0;
+						matriceMulti[ligne, colonne] = 0;
 						for (int i = 0; i <= matrice1.GetLength(1) - 1; i += (1))
 						{
-							matriceMulti[ligne, colonne] +== matrice1[ligne, i] * matrice2[i, colonne];
+							matriceMulti[ligne, colonne] += matrice1[ligne, i] * matrice2[i, colonne];
 						}
 					}
 				}
 			}
-			else
-			{
-				test == false;
-			}
-		}
+        }
+        public void Matrice_Transposition(int[,] matriceA, out int[,] matriceAT)
+        {
+
+            matriceAT = new int[matriceA.GetLength(1), matriceA.GetLength(0)]; // je crée une matrice dont le nombre de ligne est égal au nombre de colonne de la matrice donnée
+            for (int ligne = 0; ligne <= matriceA.GetLength(1) - 1; ligne += (1))
+            {
+                for (int colonne = 0; colonne <= matriceA.GetLength(0) - 1; colonne += (1))
+                {
+                    matriceAT[ligne, colonne] = matriceA[colonne, ligne];
+                }
+            }
+        }
+        public void LireReel(String question, out double n)
+        {
+            String nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!double.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention ! vous devez taper un nombre réel !");
+                Console.WriteLine("Votre proposition : ");
+                nUser = Console.ReadLine();
+            }
+        }
+        public void LireInteger(String question, out int n)
+        {
+            String nUser;
+            Console.Write(question);
+            nUser = Console.ReadLine();
+            while (!int.TryParse(nUser, out n))
+            {
+                Console.WriteLine("Attention ! vous devez taper un nombre entier !");
+                Console.WriteLine("Votre proposition : ");
+                nUser = Console.ReadLine();
+            }
+        }
     }
 }
--- 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Program.cs
+++ 5TTi1_AppMatrice_SimonArchambeau/5TTi1_AppMatrice_SimonArchambeau/Program.cs
@@ -10,6 +10,44 @@ namespace _5TTi1_AppMatrice_SimonArchambeau
     {
         static void Main(string[] args)
         {
+            Matrices MesOutils = new Matrices();
+            bool suivi = true;
+            while (suivi)
+            {
+                Console.WriteLine("Bienvenue dans mon programme Matrice !");
+                Console.WriteLine("Tapez 1 pour transposer une matrice");
+                Console.WriteLine("Tapez 2 pour la deuxième fonction");
+                Console.WriteLine("Tapez 3 pour quitter");
+                String choix = Console.ReadLine();
+                switch (choix)     // Match Expression - can be any non-null expression
+                {
+                    case "1":             // choix de la transposition
+                        int[,] t;
+                        int[,] At;
+                        int lignes = 0;
+                        int colonnes = 0;
+                        String StTab = "";
+                        MesOutils.LireInteger("Entrez le nombre de ligne de la matrice ", out lignes);
+                        MesOutils.LireInteger("Entrez le nombre de colonne de la matrice ", out colonnes);
+                        MesOutils.Matrice_tab_alea(lignes, colonnes, out t);
+                        MesOutils.Matrice_concatenation(t, out StTab);
+                        Console.WriteLine(StTab);
+                        MesOutils.Matrice_Transposition(t, out At);
+                        MesOutils.Matrice_concatenation(At, out StTab);
+                        Console.WriteLine("Matrice transposée : ");
+                        Console.WriteLine(StTab);
+                        break;
+                    case "2":             //Choix de la seconde option
+                                        // ...
+                        break;          
+                    case "3":
+                        suivi = false;                
+                        break;          
+                    default:
+                        Console.WriteLine("Option non valide");             
+                        break;          
+                }
+            }
         }
     }
 }
